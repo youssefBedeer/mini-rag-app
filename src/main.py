bad17ece70cf9b3a740d77a5f5c9a5924a055d91
youssef <yousser1bedeer@gmail.com>
2026-04-08 18:19:19 +0200

    add middleware

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ from stores.llm.LLMProviderFactory import LLMProviderFactory
 from stores.vectordb.VectorDBProviderFactory import VectorDBProviderFactory
 from stores.llm.templates.template_parser import TemplateParser
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
-
+from utils.metrics import setup_metrics
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -50,7 +50,7 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
-
+setup_metrics(app)
 
 app.include_router(base.base_router)
 app.include_router(data.data_router)
